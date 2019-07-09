e96cd1ac812841d60f81600205b342e0f27bfeb5
Yuchen Li <ychnlgy@gmail.com>
2019-07-09 11:06:02 -0400

    Added memory efficient status

---
+++ quick-stat.py
@@ -0,0 +1,18 @@
+import argparse
+import collections
+
+from voxceleb1 import load
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", required=True)
+    args = parser.parse_args()
+
+    
+
+    counter = collections.Counter()
+    for sample in load(args.file):
+        counter[sample.uid] += 1
+    min_person_set = min(counter.values())
+    print("Minimum person-specific samples:", min_person_set)
