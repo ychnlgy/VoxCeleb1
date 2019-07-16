3d37bbf1e57f1c8e31e702ccb2a114dc04beff50
Yuchen Li <ychnlgy@gmail.com>
2019-07-16 11:54:53 -0400

    Sample now adjusts for different encoding of raw audio

--- voxceleb1/preprocess/Sample.py
+++ voxceleb1/preprocess/Sample.py
@@ -30,7 +30,8 @@ class Sample:
         return Sample(*obj)
 
     def load(self):
-        self.rate, self.data = scipy.io.wavfile.read(self.fpath)
+        rate, data = scipy.io.wavfile.read(self.fpath)
+        self.rate, self.data = self._correct_raw_audio(rate, data)
         assert self.rate == EXPECTED_RATE, "File: %s has rate %d" % (
             self.fpath, self.rate
         )
@@ -47,3 +48,26 @@ class Sample:
         real = spec[:NPERSEG//2]
         self.spec = numpy.log10(1+real).astype(numpy.float16)
         self.data = None  # release memory
+
+    def _correct_raw_audio(self, rate, data):
+        # Handle multi channel, different dtypes
+        iinfo = numpy.iinfo(data.dtype)
+        r = iinfo.max - iinfo.min
+        data = (
+            data.astype(numpy.float32).mean(axis=-1) - iinfo.min
+        ) / r * 2 - 1
+
+        if rate != EXPECTED_RATE:
+
+            # Handle different rates
+            frac = EXPECTED_RATE/rate
+            n = len(data)
+            N = int(2**(numpy.floor(numpy.log2(n))+1))
+            buf = numpy.zeros(N)
+            buf[:n] = data
+            M = int(numpy.ceil(frac * N))
+            m = int(round(frac * n))
+            resampled = scipy.signal.resample(buf, M)
+            rate, data = EXPECTED_RATE, resampled[:m]
+
+        return rate, data
